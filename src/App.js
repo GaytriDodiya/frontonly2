fc3475b32b99dc45dee03cedcda5c1f93dfdcf6e
Gaytri Dodiya <dodiyadsvv@gmail.com>
2023-08-16 18:05:52 +0530

    updating build

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1>Front</h1>
+        <h1>Site</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
